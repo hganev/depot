bf70ca1d904af8b699abac7c28d65c3d3a82e689
Hristo Ganev <Hristo.b.Ganev@gmail.com>
2014-01-23 16:44:27 +0100

    add to cart button + exercises

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,16 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    @counter = set_counter
+  end
+
+  private 
+
+  def set_counter
+    if (session[:counter].nil?)
+      session[:counter] = 1
+    else
+      session[:counter] += 1
+    end
   end
 end
