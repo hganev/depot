63c8fcc6345b1e3bfd12849f0a04928cd26f940a
Hristo Ganev <Hristo.b.Ganev@gmail.com>
2014-02-02 23:26:02 +0100

    checkout implementation + atom feeds + who_bought support

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -13,6 +13,6 @@ class StoreControllerTest < ActionController::TestCase
   test "markup need for store.js.coffee is in place" do 
     get :index
     assert_select '.store .entry > img', 3
-    assert_select '.entry input[type=submit', 3
+    assert_select '.entry input[type=submit]', 3
   end
 end
