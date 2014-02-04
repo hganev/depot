a7863eed4a9e27bf1211d9e0e9d77cb9d2a0519c
Hristo Ganev <Hristo.b.Ganev@gmail.com>
2014-02-04 14:20:10 +0100

    add basic authorization

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,8 @@ class OrdersController < ApplicationController
   include CurrentCart
   before_action :set_cart, only: [:new, :create]
   before_action :set_order, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authorize, only: [:create, :new]
+
 
   # GET /orders
   # GET /orders.json
